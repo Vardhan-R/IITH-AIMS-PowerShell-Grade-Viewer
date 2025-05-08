07bb385ea19bb6275b28ba78acd9017a087e9b89
Vardhan <es22btech11031@iith.ac.in>
2025-05-08 18:27:36 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# IITH-AIMS-PowerShell-Grade-Viewer
-View all Your Grades
+# IITH AIMS PowerShell Grade Viewer
+
+Gets the user's grades from the [AIMS website for IITH](https://aims.iith.ac.in/aims/).
+
+## Requirements
+
+- [PowerShell](https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7.5)
+
+## Installation
+
+1. Download this folder (or just the [`get_all_grades.ps1`](https://github.com/Vardhan-R/IITH-AIMS-Exploitation/blob/main/grade_viewer/get_all_grades.ps1) file will suffice). [[1]](https://github.com/Vardhan-R/IITH-AIMS-Exploitation/tree/main/grade_viewer#notes)
+
+## Usage
+
+### Obtaining Your Cookie
+
+1. Log in to [the AIMS website](https://aims.iith.ac.in/aims/).
+
+2. Follow the steps in the image below to obtain your `JSESSIONID` cookie.
+![cookie instructions](https://github.com/Vardhan-R/IITH-AIMS-Exploitation/blob/main/grade_viewer/images/JSESSIONID_cookie.png)
+
+3. Make sure that your cookie doesn't expire (don't log out, nor get logged out of the AIMS website).
+
+### Running the Script (may differ on OSs other than Windows)
+
+1. Open PowerShell (run as administrator).
+
+2. Navigate to the folder containing the file `get_all_grades.ps1`.
+
+3. Run `Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force`. (This needs to be run only once per user, and PowerShell need not be run as administrator during future uses.) [[2]](https://github.com/Vardhan-R/IITH-AIMS-Exploitation/tree/main/grade_viewer#notes)
+
+4. Run `.\get_all_grades.ps1 [YOUR COOKIE]`. For example, `.\get_all_grades.ps1 1D713515E379202AD3BD5E55C967076A`.
+
+## Expected Output
+
+1. The list of your courses which have not been graded (course code, course name and academic period name).
+
+2. Your graded courses (grade-wise).
+
+## Notes
+
+1. [`get_all_grades.ps1`](https://github.com/Vardhan-R/IITH-AIMS-Exploitation/blob/main/grade_viewer/get_all_grades.ps1) renders the coloured text on all versions of PowerShell, whereas [`get_all_grades_7.ps1`](https://github.com/Vardhan-R/IITH-AIMS-Exploitation/blob/main/grade_viewer/get_all_grades_7.ps1) does the same on versions 7 or newer of PowerShell.
+
+2. You can read more about the execution policies [here](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-executionpolicy?view=powershell-7.5).
---
+++ get_all_grades.ps1
@@ -0,0 +1,125 @@
+# Requirement: PowerShell (any version)
+
+$cookie = $args[0]
+
+$gid_grd = @{
+    1  = "A+"
+    2  = "A"
+    3  = "A-"
+    4  = "B"
+    5  = "B-"
+    6  = "C"
+    7  = "C-"
+    8  = "D"
+    9  = "FS"
+    10 = "FR"
+    11 = "I"
+    12 = "S"
+    13 = "U"
+    14 = "AU"
+    15 = "F"
+    16 = "P"
+}
+
+function Display-Content {
+    param (
+        [Parameter(Mandatory = $false)]
+        $content,
+
+        [Parameter(Mandatory = $true)]
+        [string]$heading
+    )
+
+    for ($i = 0; $i -lt $content.Length; $i++) {
+        if ($i -eq 0) {
+            Write-Host $heading -ForegroundColor blue
+        }
+
+        $course = $content[$i]
+        $courseCd = $course.courseCd
+        $courseName = $course.courseName
+        $periodName = $course.periodName
+
+        Write-Host $courseCd -ForegroundColor green -NoNewLine
+        Write-Host " | " -NoNewLine
+        Write-Host $courseName -ForegroundColor magenta -NoNewLine
+        Write-Host " | " -NoNewLine
+        Write-Host $periodName -ForegroundColor cyan
+
+        if ($i -eq $content.Count - 1) {
+            Write-Host ""
+        }
+    }
+}
+
+function Get-Json {
+    param (
+        [Parameter(Mandatory = $true)]
+        $session,
+
+        [Parameter(Mandatory = $true)]
+        [string]$uri
+    )
+
+    $ProgressPreference = "SilentlyContinue"
+    $res = Invoke-WebRequest -UseBasicParsing -Uri $uri `
+    -WebSession $session `
+    -Headers @{
+        "Accept" = "*/*"
+        "Accept-Encoding" = "gzip, deflate, br, zstd"
+        "Accept-Language" = "en-US,en;q=0.9"
+        "Referer" = "https://aims.iith.ac.in/aims/courseReg/myCrsHistoryPage?dbTbNm=mn&isMnDb=false"
+        "Sec-Fetch-Dest" = "empty"
+        "Sec-Fetch-Mode" = "cors"
+        "Sec-Fetch-Site" = "same-origin"
+        "X-Requested-With" = "XMLHttpRequest"
+        "sec-ch-ua" = '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"'
+        "sec-ch-ua-mobile" = "?0"
+        "sec-ch-ua-platform" = '"Windows"'
+    } `
+    -ContentType "application/x-www-form-urlencoded; charset=UTF-8"
+    $ProgressPreference = "Continue"
+
+    $statusCode = $res.StatusCode
+    if ($statusCode -ne 200) {
+        Write-Host "Something went wrong (HTTP status code $statusCode)."
+        Write-Host "Exiting..."
+        exit
+    }
+
+    try {
+        return ConvertFrom-Json($res.Content)
+    }
+    catch {
+        Write-Host "Something went wrong. Could not convert the response content to JSON."
+        Write-Host "Exiting..."
+        exit
+    }
+}
+
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
+$session.Cookies.Add((New-Object System.Net.Cookie("JSESSIONID", $cookie, "/", "aims.iith.ac.in")))
+
+# Courses which have not been graded
+# resultIds==1 <==> passed the course, isGradeIds==2 <==> failed the course
+$uri = "https://aims.iith.ac.in/aims/courseReg/loadMyCoursesHistroy?studentId=&courseCd=&courseName=&orderBy=1&degreeIds=&acadPeriodIds=&regTypeIds=&gradeIds=&resultIds=[1]&isGradeIds="
+$passed_courses = Get-Json -session $session -uri $uri
+$uri = "https://aims.iith.ac.in/aims/courseReg/loadMyCoursesHistroy?studentId=&courseCd=&courseName=&orderBy=1&degreeIds=&acadPeriodIds=&regTypeIds=&gradeIds=&resultIds=[2]&isGradeIds="
+$failed_courses = Get-Json -session $session -uri $uri
+$graded_courses = $passed_courses + $failed_courses
+$graded_ids = $graded_courses | ForEach-Object { "$($_.courseCd)|$($_.periodName)" }
+
+$uri = "https://aims.iith.ac.in/aims/courseReg/loadMyCoursesHistroy?studentId=&courseCd=&courseName=&orderBy=1&degreeIds=&acadPeriodIds=&regTypeIds=&gradeIds=&resultIds=&isGradeIds="
+$all_courses = Get-Json -session $session -uri $uri
+$not_graded_courses = $all_courses | Where-Object { $graded_ids -notcontains "$($_.courseCd)|$($_.periodName)" }
+Display-Content -content $not_graded_courses -heading "Not graded:"
+
+# Grade-wise
+for ($gradeId = 1; $gradeId -le 16; $gradeId++) {
+    $grade = $gid_grd[$gradeId]
+
+    $uri = "https://aims.iith.ac.in/aims/courseReg/loadMyCoursesHistroy?studentId=&courseCd=&courseName=&orderBy=1&degreeIds=&acadPeriodIds=&regTypeIds=&gradeIds=[$gradeId]&resultIds=&isGradeIds="
+    $content = Get-Json -session $session -uri $uri
+    Display-Content -content $content -heading "$grade in:"
+}
